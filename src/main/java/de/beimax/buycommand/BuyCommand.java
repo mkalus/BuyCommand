fffdd28f825e008b71990aa1228072fb592e138c
Max Kalus <max@beimax.de>
2012-01-05 15:01:57 +0100

    Initial commit.

---
+++ src/main/java/de/beimax/buycommand/BuyCommand.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package de.beimax.buycommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author mkalus
+ *
+ */
+public class BuyCommand extends JavaPlugin {
+
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.Plugin#onEnable()
+	 */
+	public void onEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.Plugin#onDisable()
+	 */
+	public void onDisable() {
+		// TODO Auto-generated method stub
+
+	}
+}
